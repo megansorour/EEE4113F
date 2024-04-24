d91d1dae8d321420b49173f81ee44b438e262748
Megan Sorour <srrmeg001@myuct.ac.za>
2024-04-24 18:47:57 +0200

    Added simulaton for data processing subsystem. Timestamp still not working. Added v3 to test different softAP method to allow multiple users to acces

--- BetaTest/BetaTest.ino
+++ BetaTest/BetaTest.ino
@@ -5,7 +5,6 @@
 #include <ESPAsyncWebServer.h>
 #include "SPIFFS.h"
 #include <FS.h>
-//#include <sys/stat.h> //for accessing file info and file-related operations
 
 const char* logFileName = "/data.txt";
 
@@ -15,17 +14,11 @@ const char *password = "yourPassword7";
 const char* PARAM_INPUT_1 = "output";
 const char* PARAM_INPUT_2 = "state";
 const char* PARAM_INPUT_3 = "weight";
-const char* PARAM_MESSAGE = "message";
 
 String currentWeight = "0";
-String timestamp = "0";
-bool newTimestamp = false;
+String timestamp;
 
 int sim = 0;
-
-//values for simulation
-int bird_there_sim[12]  = {0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1};
-bool birdThere = false;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -87,16 +80,9 @@ const char index_html[] PROGMEM = R"rawliteral(
     <br>
     <br> -->
     <button class="button" id="downloadBtn">Download data log</button>
-  %BUTTONPLACEHOLDER%
   
 <script>
 
-function toggleCheckbox(element) {
-  var xhr = new XMLHttpRequest();
-  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
-  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
-  xhr.send();
-}
   function logWeight() {
     // logging the measurement to the data array
     dataLog.push(document.getElementById('datetime').innerText);
@@ -118,57 +104,27 @@ const now = new Date();
 const currentTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 document.querySelector('#datetime').textContent = currentTime;
 
-// V1 Send time to ESP32
-//   fetch('/upload', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'text/plain'
-//     },
-//     body: currentTime
-//   })
-//   .then(response => response.text())
-//   .then(data => console.log(data))
-//   .catch(error => console.error('Error sending time:', error));
+//V2
+  // Create a new XMLHttpRequest object
+  var xhr = new XMLHttpRequest();
 
-// //   // Update time on the webpage
-// //   document.querySelector('#time').textContent = currentTime;
-     
+  // Configure the request
+  xhr.open("POST", "/upload", true);
+  xhr.setRequestHeader("Content-Type", "text/plain");
 
-// //V2
-//   // Create a new XMLHttpRequest object
-//   var xhr = new XMLHttpRequest();
+  // Send the timestamp
+  xhr.send(currentTime);
 
-//   // Configure the request
-//   xhr.open("POST", "/upload", true);
-//   xhr.setRequestHeader("Content-Type", "text/plain");
-
-//   // Send the timestamp
-//   xhr.send(currentTime);
-
-//   // Handle the response
-//   xhr.onreadystatechange = function () {
-//     if (xhr.readyState === 4) {
-//       if (xhr.status === 200) {
-//         console.log("Timestamp sent successfully");
-//       } else {
-//         console.error("Failed to send timestamp");
-//       }
-//     }
-//   };
-
-//V3
-// Send time to ESP32
-console.log('Sending time:', currentTime); // Add this line
-  fetch('/upload', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'text/plain'
-    },
-    body: currentTime
-  })
-  .then(response => response.text())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error sending time:', error));
+  // Handle the response
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log("Timestamp sent successfully");
+      } else {
+        console.error("Failed to send timestamp");
+      }
+    }
+  };
   }
 
 // Update weight and send time to ESP32 every 5 seconds
@@ -198,26 +154,6 @@ setInterval(updateWeight, 5000);
 </body>
 </html>
 )rawliteral";
-
-// Replaces placeholder with button section in your web page
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "BUTTONPLACEHOLDER"){
-    String buttons = "";
-    buttons += "<h4>Output - Blue LED</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"2\" " + outputState(2) + "><span class=\"slider\"></span></label>";
-    return buttons;
-  }
-  return String();
-}
-
-String outputState(int output){
-  if(digitalRead(output)){
-    return "checked";
-  }
-  else {
-    return "";
-  }
-}
 
   // If webpage requested does not exist
   void notFound(AsyncWebServerRequest *request) {
@@ -249,28 +185,7 @@ void setup(){
 
     // Route for root / web page (navigating to html thing defined as index_html) (home page)
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-
-  // LED: Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputMessage1;
-    String inputMessage2;
-    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-    if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
-      inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
-      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
-      digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
-    }
-    else {
-      inputMessage1 = "No message sent";
-      inputMessage2 = "No message sent";
-    }
-    Serial.print("GPIO: ");
-    Serial.print(inputMessage1);
-    Serial.print(" - Set to: ");
-    Serial.println(inputMessage2);
-    request->send(200, "text/plain", "OK");
+    request->send_P(200, "text/html", index_html);
   });
 
   // Set up the endpoint to return the current weight value
@@ -292,47 +207,19 @@ server.on("/download", HTTP_GET, [](AsyncWebServerRequest *request){
     file.close();
   });
 
-
-//V1receiving the current timestamp from the server, manually parsing JSON
-// server.on("/upload", HTTP_POST, [](AsyncWebServerRequest *request){
-//   // Check if the request has a "plain" parameter
-//   if (!request->hasParam("plain", true)) {
-//     request->send(400, "text/plain", "Missing timestamp");
-//     return;
-//   }
-
-//   // Get the value of the "plain" parameter (the timestamp)
-//   AsyncWebParameter* plainParam = request->getParam("plain", true);
-//   if (!plainParam->isPost()) {
-//     request->send(400, "text/plain", "Invalid request");
-//     return;
-//   }
-
-//   // Get the timestamp value as a string
-//   timestamp = plainParam->value();
-
-//   // Handle the timestamp (e.g., store it with the weights)
-//   // Here, we simply respond with a success message
-//   request->send(200, "text/plain", "Timestamp received: " + timestamp);
-//   Serial.println(timestamp);
-// });
-
   server.on("/upload", HTTP_POST, [](AsyncWebServerRequest *request){
     // Get the timestamp value from the request body
     String currentTime;
     if (request->hasParam("plain", true)) {
-      //AsyncWebParameter* plainParam = request->getParam("plain", true);
-      currentTime = request->getParam("plain", true)->value();
-    
+      AsyncWebParameter* plainParam = request->getParam("plain", true);
+      currentTime = plainParam->value();
+    }
+
     // Store the timestamp in the global variable
     timestamp = currentTime;
-    newTimestamp = true;
-    }
-    else {
-      currentTime = "No time sent";
-    }
+
     // Respond with a success message
-    request->send(200, "text/plain", "Timestamp received: " + currentTime);
+    request->send(200, "text/plain", "Timestamp received: " + timestamp);
     Serial.println(timestamp);
   });
 
@@ -351,49 +238,33 @@ server.on("/download", HTTP_GET, [](AsyncWebServerRequest *request){
 
 void loop() {
   //simulating some kind of data monitoring function which produces a weight value
+  currentWeight = "0";
+  if (sim == 0 || sim == 3 || sim == 5 || sim == 6 || sim == 8 || sim == 10 || sim == 11){
         delay(5000);
-        birdThere = isBird();
-        if (birdThere){
-          currentWeight = getWeight();
-          if (newTimestamp){
-            //opening the log file in append mode
-            File logFile = SPIFFS.open(logFileName, "a");
+        currentWeight = random(500); //weight value
 
-            if (logFile){
-              //append weight and time to log file
-              logFile.print(timestamp);
-              logFile.print(", ");
-              logFile.print(currentWeight);
-              logFile.println();
-            }
-            else {
-              Serial.println("Failed to open log file for writing.");
-            }
-            logFile.close();
-            newTimestamp = false; // Reset the flag
-          }
+        //opening the log file in append mode
+        File logFile = SPIFFS.open(logFileName, "a");
+
+        if (logFile){
+          //append weight and time to log file
+          logFile.print(timestamp);
+          logFile.print(", ");
+          logFile.print(currentWeight);
+          logFile.println();
         }
         else {
-          currentWeight = "0";
+          Serial.println("Failed to open log file for writing.");
         }
-  sim ++;
-  delay(1000);
-
-}
-
-//functions to simulate data processing subsystem
-bool isBird(){
-  delay(100);
-  if (bird_there_sim[sim]==0){
-    return false;
+        logFile.close();
   }
   else {
-    return true;
+    currentWeight = "0";
   }
-}
+  sim ++;
+  delay(5000);
+  if (sim == 13){
+    sim = 0;
+  }
 
-String getWeight(){
-  delay(1000);
-  currentWeight = random(500); //random weight value
-  return currentWeight;
 }
